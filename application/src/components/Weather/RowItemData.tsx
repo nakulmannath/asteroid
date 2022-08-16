7118869c799b81e397948668b904799a66012a38
Nakul Sagar <nakul.sagar@hyperrealitytech.com>
2022-08-16 11:27:38 +0530

    asteroid app

--- application/src/components/Weather/RowItemData.tsx
+++ application/src/components/Weather/RowItemData.tsx
@@ -1,28 +1,21 @@
 import { View, Text, TextStyle, ViewStyle } from "react-native";
 import { spacing, color } from "../../theme";
 
-const ROW_CONTAINER: ViewStyle = {
-  flexDirection: "row",
-  alignItems: "center",
-  marginVertical: spacing[1],
-  marginRight: spacing[8],
-};
 const ITEM_CONTAINER: ViewStyle = {
   marginVertical: spacing[4],
   marginLeft: spacing[0],
   marginRight: spacing[6],
-  // width: Dimensions.get("window").width - 30,
 };
 const LABEL: TextStyle = {
   fontSize: 14,
   color: color.palette.black,
   marginEnd: spacing[2],
-  marginHorizontal: spacing[-2],
+  paddingBottom: spacing[2],
 };
 const VALUE: TextStyle = {
   fontSize: 12,
-  color: color.palette.black,
-  marginRight: spacing[6],
+  color: "red",
+  paddingBottom: spacing[6],
 };
 
 const RowDataItem = ({
@@ -36,15 +29,15 @@ const RowDataItem = ({
 }) => {
   return (
     <View style={ITEM_CONTAINER}>
-      <View style={ROW_CONTAINER}>
-        <Text style={LABEL}>{"Name: "}</Text>
+      <View>
+        <Text style={LABEL}>{"Name : "}</Text>
         <Text style={VALUE}>{name}</Text>
       </View>
-      <View style={ROW_CONTAINER}>
-        <Text style={LABEL}>{"NASA_JPL_URL: "}</Text>
+      <View>
+        <Text style={LABEL}>{"NASA_JPL_URL : "}</Text>
         <Text style={VALUE}>{nasa_jpl_url}</Text>
       </View>
-      <View style={ROW_CONTAINER}>
+      <View>
         <Text style={LABEL}>{"Is_Potentially_Hazardous_Asteroid: "}</Text>
         <Text style={VALUE}>
           {is_potentially_hazardous_asteroid.toString()}
